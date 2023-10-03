4c1a58318ba2bee6a8a4a9f99b28280b6746e155
Katsiaryna <katsiaryna.n@gmail.com>
2023-10-03 11:05:20 +0300

    feat: add API request

---
+++ my-first-react-app/src/api.ts
@@ -0,0 +1,11 @@
+import { ResultsData } from './types';
+
+const BaseURL = 'https://swapi.dev/api/starships/';
+
+export const searchRequest = async (searchValue: string): Promise<{ results: ResultsData[] }> => {
+  const url = `${BaseURL}?search=${searchValue}`;
+  const resp = await fetch(url);
+  const result = await resp.json();
+  console.log(result);
+  return result;
+};
