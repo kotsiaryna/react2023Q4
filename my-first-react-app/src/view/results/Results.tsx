80f941ab221070252ee8c50d69b8a9f056c3ab19
Katsiaryna <katsiaryna.n@gmail.com>
2023-10-03 12:31:23 +0300

    feat: add rendering default results

---
+++ my-first-react-app/src/view/results/Results.tsx
@@ -0,0 +1,30 @@
+import { Component } from 'react';
+import { searchRequest } from '../../api';
+import { ResultsData } from '../../types';
+import Ship from './Ship';
+
+class Results extends Component {
+  declare props: { searchValue: string };
+
+  state: Readonly<{ results: ResultsData[] }> = {
+    results: [{ name: '', model: '', length: '' }],
+  };
+
+  render() {
+    return (
+      <section className="results">
+        {this.state.results.map((res, i) => {
+          return <Ship key={i} name={res.name} model={res.model} length={res.length} />;
+        })}
+      </section>
+    );
+  }
+  async componentDidMount(): Promise<void> {
+    const searchData = await searchRequest(this.props.searchValue);
+    const results = searchData.results;
+    this.setState({
+      results: results,
+    });
+  }
+}
+export default Results;
