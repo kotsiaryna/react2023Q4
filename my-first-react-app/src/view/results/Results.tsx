38ec01dba196a70034b7d20a7c8d236501c68bc0
Katsiaryna <katsiaryna.n@gmail.com>
2023-10-05 10:33:33 +0300

    feat: add loader to Result section

--- my-first-react-app/src/view/results/Results.tsx
+++ my-first-react-app/src/view/results/Results.tsx
@@ -6,35 +6,58 @@ import Ship from './Ship';
 class Results extends Component {
   declare props: { searchValue: string };
 
-  declare state: Readonly<{ results: ResultsData[] }>;
+  state: Readonly<{ results: ResultsData[] | null; isLoading: boolean }>;
 
-  showContent = (st: { results: ResultsData[] }) => {
-    if (st) {
-      return st.results.map((res, i) => {
+  constructor(props: object) {
+    super(props);
+    // this.state = { results: [{ name: '', model: '', length: '' }], isLoading: false };
+    this.state = { results: null, isLoading: false };
+  }
+
+  showContent = (results: ResultsData[] | null) => {
+    if (!results) return;
+
+    if (results.length) {
+      return results.map((res, i) => {
         return <Ship key={i} name={res.name} model={res.model} length={res.length} />;
       });
     } else {
-      return '';
+      return <div>No matches</div>;
     }
   };
 
   render() {
-    return <section className="results">{this.showContent(this.state)}</section>;
+    // console.log('render()');
+    console.log('rendering Results');
+    return (
+      <section className="results">
+        {this.state.isLoading && <p>Loading...</p>}
+        {this.showContent(this.state.results)}
+      </section>
+    );
   }
 
   getData = async () => {
+    // console.log('getting data...');
+    this.setState({
+      isLoading: true,
+      results: null,
+    });
     const searchData = await searchRequest(this.props.searchValue);
     const results = searchData.results;
     this.setState({
       results: results,
+      isLoading: false,
     });
   };
 
   async componentDidMount(): Promise<void> {
+    // console.log('didMount');
     await this.getData();
   }
 
   async componentDidUpdate(prevProps: Readonly<{ searchValue: string }>): Promise<void> {
+    // console.log('didUpdate');
     if (prevProps.searchValue !== this.props.searchValue) {
       await this.getData();
     }
