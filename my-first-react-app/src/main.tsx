6b4e089bfa2b3ac3b3e2d3834f291c3379ceacce
Katsiaryna <katsiaryna.n@gmail.com>
2023-10-10 14:39:35 +0300

    feat: add router

--- my-first-react-app/src/main.tsx
+++ my-first-react-app/src/main.tsx
@@ -2,9 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './view/App';
 import './index.scss';
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
+import Results from './view/results/Results';
+import { searchRequest } from './api';
+import ResultsError from './view/results/ResultsError';
+import Page404 from './view/notFound/Page404';
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    // TODO errorElement for app = '404 Page not found'
+    <Route path="/" element={<App />} errorElement={<Page404 />}>
+      <Route
+        path=":search/:page"
+        element={<Results />}
+        loader={searchRequest}
+        errorElement={<ResultsError />}
+      />
+    </Route>
+  )
+);
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
