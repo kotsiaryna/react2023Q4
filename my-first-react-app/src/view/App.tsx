8eff34d85dce73b3b50cbce7544281b3598e1b3f
Katsiaryna <katsiaryna.n@gmail.com>
2023-10-11 15:47:12 +0300

    feat: add pagination

--- my-first-react-app/src/view/App.tsx
+++ my-first-react-app/src/view/App.tsx
@@ -1,24 +1,26 @@
 import { ReactNode, useEffect, useState } from 'react';
 import Search from './search/Search';
 import { Outlet, useParams } from 'react-router-dom';
+import Pagination from './results/Pagination';
 
 const App = (): ReactNode => {
-  const { search } = useParams();
+  const { search, page } = useParams();
 
   const [isLoading, setIsLoading] = useState(true);
 
-  const changeLoadingState = (value: string) => {
-    if (value !== search) setIsLoading(true); // check if search has changed
+  const changeLoadingState = (value?: string, pageNumber?: string) => {
+    if (value !== search || pageNumber !== page) setIsLoading(true); // check if search has changed
   };
 
   useEffect(() => {
     setIsLoading(false);
-  }, [search]);
+  }, [search, page]);
 
   console.log(isLoading);
   return (
     <>
       <Search handleClick={changeLoadingState} />
+      <Pagination handleClick={changeLoadingState} />
       {isLoading ? <div className="results__preload"></div> : <Outlet />}
     </>
   );
---
+++ my-first-react-app/src/view/results/Pagination.tsx
@@ -0,0 +1,45 @@
+import { MouseEventHandler, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+
+function Pagination(props: { handleClick: (value: string) => void }) {
+  // TODO disable links on first and last pages
+  const { page, search } = useParams();
+  const [curPage, setCurPage] = useState(page);
+
+  const handleBackClick: MouseEventHandler = (e) => {
+    if (page === '1') {
+      e.preventDefault();
+    } else {
+      setCurPage(`${Number(curPage) - 1}`);
+    }
+  };
+
+  const handleForwardClick: MouseEventHandler = () => {
+    setCurPage(`${Number(curPage) + 1}`);
+  };
+  return (
+    <section className="pagination">
+      <Link
+        to={`/${search}/${page === '1' ? page : Number(page) - 1}`}
+        onClick={(e) => {
+          handleBackClick(e);
+          if (curPage !== '1') props.handleClick(String(curPage));
+        }}
+      >
+        BACK
+      </Link>
+      <div className="pageNumber">{curPage}</div>
+      <Link
+        to={`/${search}/${Number(page) + 1}`}
+        onClick={(e) => {
+          handleForwardClick(e);
+          props.handleClick(String(curPage));
+        }}
+      >
+        FORWARD
+      </Link>
+    </section>
+  );
+}
+
+export default Pagination;
