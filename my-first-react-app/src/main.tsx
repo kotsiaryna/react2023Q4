dfab4b9aa1363cbb4823dfa32bba3e996024aa4e
Katsiaryna <katsiaryna.n@gmail.com>
2023-11-05 15:07:11 +0300

    feat: remove Error Boundary component

--- my-first-react-app/src/main.tsx
+++ my-first-react-app/src/main.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './view/App';
-import './index.scss';
 import {
   Route,
   RouterProvider,
   createBrowserRouter,
   createRoutesFromElements,
 } from 'react-router-dom';
+
+import App from './view/App';
 import Results from './view/results/Results';
-import { articleRequest } from './api';
 import Page404 from './view/notFound/Page404';
-import ErrorBoundary from './view/ErrorBoundary';
 import ArticleDetails from './view/results/ArticleDetails';
 import ErrorPage from './view/error/ErrorPage';
 
+import { articleRequest } from './api';
+
+import './index.scss';
+
 const router = createBrowserRouter(
   createRoutesFromElements(
-    // TODO errorElements for different errors
     <Route path="/" element={<App />} errorElement={<ErrorPage />}>
       <Route path=":search/:page" element={<Results />}>
         <Route path=":id" element={<ArticleDetails />} loader={articleRequest} />
@@ -28,8 +29,6 @@ const router = createBrowserRouter(
 );
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <RouterProvider router={router} />
-    </ErrorBoundary>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
