1ef81ac78008fc21fcd8ecc47647e01b39b3e76b
Katsiaryna <katsiaryna.n@gmail.com>
2023-11-10 15:03:31 +0300

    refactor: restructure router config

--- my-first-react-app/src/main.tsx
+++ my-first-react-app/src/main.tsx
@@ -1,34 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from 'react-router-dom';
-
-import App from './view/App';
-import Results from './view/results/Results';
-import Page404 from './view/notFound/Page404';
-import ArticleDetails from './view/results/ArticleDetails';
-import ErrorPage from './view/error/ErrorPage';
-
-import { articleRequest } from './api';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import routesConfig from './routerConfig';
 
 import './index.scss';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />} errorElement={<ErrorPage />}>
-      <Route path=":search/:page" element={<Results />}>
-        <Route path=":id" element={<ArticleDetails />} loader={articleRequest} />
-      </Route>
-      <Route path="*" element={<Page404 />} />
-    </Route>
-  )
-);
+const router = createBrowserRouter(routesConfig);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
 );
+export { routesConfig };
