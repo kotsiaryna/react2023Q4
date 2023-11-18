58b4fb8f7cd660d6b824ed9cf3b1b1dc43cf80be
Katsiaryna <katsiaryna.n@gmail.com>
2023-11-18 17:08:18 +0300

    feat: add Pagination tests

--- my-first-react-app/__tests__/Pagination.test.tsx
+++ my-first-react-app/__tests__/Pagination.test.tsx
@@ -3,14 +3,33 @@ import { fireEvent, render, screen } from '@testing-library/react';
 
 import { BrowserRouter } from 'react-router-dom';
 import Pagination from '../src/view/results/Pagination';
+import { setupStore } from '../src/redux/store';
+import { Provider } from 'react-redux';
 
 describe('Pagination', () => {
+  const initialState = {
+    searchValue: '',
+    itemsPerPage: '10',
+    flags: {
+      isLoadingResults: false,
+      isLoadingPage: true,
+      isLoadingDetails: false,
+    },
+  };
+  let store = setupStore(initialState);
   const startPageNumber = 2;
+
+  beforeEach(() => {
+    store = setupStore(initialState);
+  });
+
   it('URL is updated when page changes (BACK)', () => {
     render(
-      <BrowserRouter>
-        <Pagination handleClick={jest.fn()} totalAmount={50} limit={10} page={startPageNumber} />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Pagination totalAmount={50} limit={10} page={startPageNumber} />
+        </BrowserRouter>
+      </Provider>
     );
     const backButton = screen.getByText(/back/i);
     fireEvent.click(backButton);
@@ -19,11 +38,28 @@ describe('Pagination', () => {
     expect(currentPage).toBe(`${startPageNumber - 1}`);
   });
 
+  it('Nothing happens when click back on first page (BACK)', () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Pagination totalAmount={50} limit={10} page={1} />
+        </BrowserRouter>
+      </Provider>
+    );
+    const backButton = screen.getByText(/back/i);
+    fireEvent.click(backButton);
+    const path = window.location.pathname;
+    const currentPage = path.split('/').at(2);
+    expect(currentPage).toBe('1');
+  });
+
   it('URL is updated when page changes (FORWARD)', () => {
     render(
-      <BrowserRouter>
-        <Pagination handleClick={jest.fn()} totalAmount={50} limit={10} page={startPageNumber} />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Pagination totalAmount={50} limit={10} page={startPageNumber} />
+        </BrowserRouter>
+      </Provider>
     );
     const forwardButton = screen.getByText(/forward/i);
     fireEvent.click(forwardButton);
