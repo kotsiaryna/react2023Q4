3ec12bafc8f601d366f89d2ace4bea44f7a20e69
Katsiaryna <katsiaryna.n@gmail.com>
2023-11-18 22:48:02 +0300

    feat: instal and config msw

--- my-first-react-app/jest.config.cjs
+++ my-first-react-app/jest.config.cjs
@@ -2,11 +2,23 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  testEnvironmentOptions: {
+    customExportConditions: [''],
+  },
   transform: {
     '^.+\\.ts?$': 'ts-jest',
   },
+  setupFiles: ['./jest.polyfills.js'],
   testRegex: '((\\.|/*.)(test))\\.tsx?$',
   moduleNameMapper: {
     '^.+\\.(css|scss|png)$': 'babel-jest',
+  },
+  globals: {
+    fetch,
+    Headers,
+    Request,
+    Response,
+    FormData,
+    Blob,
   },
 };
