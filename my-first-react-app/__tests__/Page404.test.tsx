a1230788555494e7a87d8300ce34b0bf279ee2a2
Katsiaryna <katsiaryna.n@gmail.com>
2023-11-18 16:49:32 +0300

    feat: Page404 tests

--- my-first-react-app/__tests__/Page404.test.tsx
+++ my-first-react-app/__tests__/Page404.test.tsx
@@ -2,14 +2,31 @@ import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import routesConfig from '../src/routerConfig';
 import { createMemoryRouter, RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { setupStore } from '../src/redux/store';
 
 describe('Page 404', () => {
+  const initialState = {
+    searchValue: '',
+    itemsPerPage: '10',
+    flags: {
+      isLoadingResults: false,
+      isLoadingPage: true,
+      isLoadingDetails: false,
+    },
+  };
+  const store = setupStore(initialState);
+
   it('Page 404 is displayed when navigating to an invalid route', async () => {
     const router = createMemoryRouter(routesConfig, {
       initialEntries: ['/badRoute'],
     });
 
-    render(<RouterProvider router={router} />);
+    render(
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    );
 
     const errorPageElement = screen.getByTestId('page404');
 
