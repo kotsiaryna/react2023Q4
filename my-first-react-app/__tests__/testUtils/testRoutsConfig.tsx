cb0cf79ff2e0855398eddd0bb4cab83527d5fcb5
Katsiaryna <katsiaryna.n@gmail.com>
2023-11-19 21:57:08 +0300

    feat: add mock data for tests

---
+++ my-first-react-app/__tests__/testUtils/testRoutsConfig.tsx
@@ -0,0 +1,20 @@
+import Page404 from '../../src/view/notFound/Page404';
+import ArticleDetails from '../../src/view/results/ArticleDetails';
+import Results from '../../src/view/results/Results';
+
+export const routesConfig = [
+  {
+    path: ':search/:page',
+    element: <Results />,
+    children: [
+      {
+        path: ':id',
+        element: <ArticleDetails />,
+      },
+    ],
+  },
+  {
+    path: '*',
+    element: <Page404 />,
+  },
+];
