4e101899bc26f29eb57fdbb84ecbd74d2a0636e5
Katsiaryna <katsiaryna.n@gmail.com>
2023-11-23 12:36:59 +0300

    feat: create store and APIwith RTK Query

---
+++ next-app/src/redux/store.ts
@@ -0,0 +1,22 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { createWrapper } from "next-redux-wrapper";
+import { newsApi } from "./api";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+
+const setupStore = () => {
+  return configureStore({
+    reducer: {
+      [newsApi.reducerPath]: newsApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(newsApi.middleware),
+  })
+}
+
+type AppStore = ReturnType<typeof setupStore>;
+type RootState = ReturnType<AppStore["getState"]>;
+type AppDispatch = AppStore["dispatch"];
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export const wrapper = createWrapper<AppStore>(setupStore, { debug: true });
