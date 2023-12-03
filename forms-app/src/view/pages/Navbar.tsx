33b0959e3ff5052714c86e333b37547092a7d7f4
Katsiaryna <katsiaryna.n@gmail.com>
2023-12-03 23:42:05 +0300

    feat: remove link

--- forms-app/src/view/pages/Navbar.tsx
+++ forms-app/src/view/pages/Navbar.tsx
@@ -6,7 +6,6 @@ const Navbar = () => {
     <main>
       <h1>{'Hello forms ;-)'}</h1>
       <nav>
-        <Link to="/"> Back on main</Link>
         <Link to="uncontrolled"> Form without control</Link>
         <Link to="controlled"> Form with control</Link>
       </nav>
